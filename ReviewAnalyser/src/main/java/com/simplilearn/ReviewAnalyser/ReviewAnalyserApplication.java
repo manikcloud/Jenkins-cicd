cf2f54ba9c1785f2935165a6865d54d73add064e
varun <you@example.com>
2022-08-27 09:29:19 +0000

    add all files 1

---
+++ ReviewAnalyser/src/main/java/com/simplilearn/ReviewAnalyser/ReviewAnalyserApplication.java
@@ -0,0 +1,13 @@
+package com.simplilearn.ReviewAnalyser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewAnalyserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewAnalyserApplication.class, args);
+	}
+
+}
