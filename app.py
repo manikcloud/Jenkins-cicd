b956400a9a0a11f573989e3ccf758e2a4d60b499
varun <varun@gmail.com>
2023-02-19 13:22:34 +0800

    date change

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Flask inside Docker by Varun Manik 2023 !!"
+    return "Flask inside Docker by Varun Manik 2023 Feb 19!!"
 
 
 if __name__ == "__main__":
