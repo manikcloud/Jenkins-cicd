c08e7f2b84e1dc53330e44479ae6d357d98236fc
varun <varun@gmail.com>
2023-02-19 14:48:49 +0800

    change address

--- src/main/java/com/mankikdemo/tutorial/addressbook/AddressbookUI.java
+++ src/main/java/com/mankikdemo/tutorial/addressbook/AddressbookUI.java
@@ -38,7 +38,7 @@ public class AddressbookUI extends UI {
      */
     TextField filter = new TextField();
     Grid contactList = new Grid();
-    Button newContact = new Button("New Registration");
+    Button newContact = new Button("Registration");
 
     // ContactForm is an example of a custom component class
     ContactForm contactForm = new ContactForm();
