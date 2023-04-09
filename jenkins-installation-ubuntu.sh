a3858fd8b3a6eb86a5f7a74bc6750f75a7eb5673
varun <varun@gmail.com>
2023-04-09 21:52:28 +0800

    adding updated command

--- jenkins-installation-ubuntu.sh
+++ jenkins-installation-ubuntu.sh
@@ -7,8 +7,10 @@ sudo apt install default-jdk -y
 
 sudo java -version
 sudo mkdir -p /usr/share/keyrings
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+sudo curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee   /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+sudo echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]   https://pkg.jenkins.io/debian-stable binary/ | sudo tee   /etc/apt/sources.list.d/jenkins.list > /dev/null
+
 sudo apt update -y
 sudo apt install jenkins -y 
 systemctl status jenkins --no-pager -l
