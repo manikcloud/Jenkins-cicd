fb8bb7fcc6a0e402fdd75902b264c3bdc51e797d
varun <varun@gmail.com>
2023-04-10 01:13:09 +0800

    adding date

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! From Varun Manik Simplilearn on Date Apr 09 2023 time 17 oclock" );
+        System.out.println( "Hello World! From Varun Manik Simplilearn on Date Apr 09 2023 time 17:15 pm" );
     }
 }
