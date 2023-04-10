ab0d259d2cf3f861147a79b6e5147398073ec0ce
varun <varun@gmail.com>
2023-04-10 14:05:43 +0800

    adding date

--- ubuntu-vm.tf
+++ ubuntu-vm.tf
@@ -55,6 +55,7 @@ resource "aws_instance" "ubuntu" {
     inline = [
       "sudo apt update ",
       "sudo apt install -y default-jdk ",
+      "sudo mkdir -p /usr/share/keyrings/",
       "sudo curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee   /usr/share/keyrings/jenkins-keyring.asc > /dev/null",
       "sudo echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]   https://pkg.jenkins.io/debian-stable binary/ | sudo tee   /etc/apt/sources.list.d/jenkins.list > /dev/null",
       "sudo apt update",
