48179d65ebbbcff0a9a88cb1656ec6072989d198
varun <varun@gmail.com>
2023-04-10 14:04:26 +0800

    adding date

--- ubuntu-vm.tf
+++ ubuntu-vm.tf
@@ -53,8 +53,8 @@ resource "aws_instance" "ubuntu" {
   # Remotely execute commands to install Java, Python, Jenkins
   provisioner "remote-exec" {
     inline = [
-      "sudo apt update && upgrade",
-      "sudo apt install -y default-jdk python3.8",
+      "sudo apt update ",
+      "sudo apt install -y default-jdk ",
       "sudo curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee   /usr/share/keyrings/jenkins-keyring.asc > /dev/null",
       "sudo echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]   https://pkg.jenkins.io/debian-stable binary/ | sudo tee   /etc/apt/sources.list.d/jenkins.list > /dev/null",
       "sudo apt update",
