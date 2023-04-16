516b8caa80914188dc98f989dae64bb245012734
varun <varun@gmail.com>
2023-04-16 14:47:11 +0800

    adding ec2 scripts

--- ec2-delete.sh
+++ ec2-delete.sh
@@ -1,22 +1,11 @@
 #!/bin/bash
 
-AMI_ID="ami-0c55b159cbfafe1f0"
-INSTANCE_TYPE="t2.micro"
-KEY_NAME="<key_pair_name>"
-TAG_NAME="awscli"
-COUNT=3
-REGION="us-east-1"
-
-aws ec2 run-instances \
-    --image-id $AMI_ID \
-    --instance-type $INSTANCE_TYPE \
-    --count $COUNT \
-    --key-name $KEY_NAME \
-    --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value='$TAG_NAME'}]' \
-    --region $REGION
-
-echo "Waiting for instances to launch..."
-sleep 20
+# AMI_ID="ami-0c55b159cbfafe1f0"
+# INSTANCE_TYPE="t2.micro"
+# KEY_NAME="<key_pair_name>"
+# TAG_NAME="awscli"
+# COUNT=3
+# REGION="us-east-1"
 
 INSTANCE_IDS=$(aws ec2 describe-instances \
     --filters "Name=tag:Name,Values=$TAG_NAME" \
@@ -24,4 +13,22 @@ INSTANCE_IDS=$(aws ec2 describe-instances \
     --output text \
     --region $REGION)
 
-echo "Instance IDs: $INSTANCE_IDS"
+echo "Terminating instances: $INSTANCE_IDS"
+aws ec2 terminate-instances --instance-ids $INSTANCE_IDS --region $REGION
+
+echo "Waiting for instances to terminate..."
+sleep 20
+
+for INSTANCE_ID in $INSTANCE_IDS; do
+    INSTANCE_STATE=$(aws ec2 describe-instances \
+        --instance-ids $INSTANCE_ID \
+        --query 'Reservations[0].Instances[0].State.Name' \
+        --output text \
+        --region $REGION)
+
+    if [ "$INSTANCE_STATE" = "terminated" ]; then
+        echo "Instance $INSTANCE_ID has been terminated."
+    else
+        echo "Instance $INSTANCE_ID has not been terminated. Something went wrong."
+    fi
+done
