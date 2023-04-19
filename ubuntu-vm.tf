55f79903ef9dc25bccc6aec7ff463030fca3d5de
varun <varun@gmail.com>
2023-04-20 01:00:16 +0800

    ansible jenkins

--- ubuntu-vm.tf
+++ ubuntu-vm.tf
@@ -1,18 +1,18 @@
-# variable "jenkins_admin_user" {
-#   description = "The admin username for Jenkins"
-#   default     = "admin"
-# }
+# Define variables for Jenkins admin user and password
+variable "jenkins_admin_user" {
+  description = "The admin username for Jenkins"
+  default     = "admin"
+}
 
-# variable "jenkins_admin_password" {
-#   description = "The admin password for Jenkins"
-#   default     = "Admin@456"
-# }
+variable "jenkins_admin_password" {
+  description = "The admin password for Jenkins"
+  default     = "Admin@456"
+}
 
+# Define security group and key pair resources
 resource "aws_security_group" "allow_SSH_ubuntu" {
   name        = "allow_SSH_ubuntu"
   description = "Allow SSH inbound traffic"
-  #   vpc_id      = aws_vpc.main.id
-
 
   ingress {
     from_port        = 0
@@ -36,9 +36,7 @@ resource "aws_key_pair" "deployer" {
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDD8gTEP0wdqJnHkVDc7IMzFygpsibTemZylZvk4gcGiaa+YX8/VpUEyOrqYOg/8OrOAsdQCpPcc8J3AhUBSvgjeKc7sJZ0N8v+AOKnVQDni/y+9mWD6oGOszzH6DVfoHOy1HRZAbF6n+xFkk0DeisSJ3FsGVQK/J5rYgXVCBJii34mL0+DOBogxLva8tDcLgWLz+qxs8QcH4hgrHkdT8whBKfKcypICGS4U/WgXFW5M3pN4OynSzeqJ9gXn0Fbq50J/t6Cpnr6wqlzRzocAMXnlgrchYZWZJgTL3W8zrg1Fx7RBy3fAbEyWoyrkB9cFIPSkmiUZG9xUhirI4MbdgHVNlkp7xU0V6KfCRlTNE+xzWlbPSga7kjlqiizjDoR7xExKNLbNthB6nlNIEwX+FljtgrTqMV2w6k8H3xfrhdG0993EnMMwdPVmltEjJeZ+l3BETUcibKGFFc5iyR99W7NKhriH5d6OvbXev7JmbqsXrwn8rnu3auDGJs7YaIwzmk= varunmanikoutlo@ip-172-31-17-206"
 }
 
-####### Ubuntu VM #####
-
-
+# Create Ubuntu VM instance
 resource "aws_instance" "ubuntu" {
   ami                    = "ami-007855ac798b5175e"
   instance_type          = "t2.medium"
@@ -48,10 +46,7 @@ resource "aws_instance" "ubuntu" {
     "Name" = "Jenkins-UBUNTU-22-04"
     "ENV"  = "Dev"
   }
-  # metadata_options {
-  #   http_endpoint = "disabled"
-  # }
-  # Type of connection to be established
+
   connection {
     type        = "ssh"
     user        = "ubuntu"
@@ -60,29 +55,21 @@ resource "aws_instance" "ubuntu" {
     timeout     = "10m"
   }
 
-    associate_public_ip_address = true
-
-  # Remotely execute commands to install Java, Python, Jenkins
   provisioner "remote-exec" {
     inline = [
-      "sudo apt update ",
-      "sudo apt install -y default-jdk ",
-      "sudo mkdir -p /usr/share/keyrings/",
-      "sudo curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee   /usr/share/keyrings/jenkins-keyring.asc > /dev/null",
-      "sudo echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]   https://pkg.jenkins.io/debian-stable binary/ | sudo tee   /etc/apt/sources.list.d/jenkins.list > /dev/null",
-      "sudo apt update",
-      "sudo apt install -y jenkins",
-      "sudo systemctl start jenkins --no-pager -l",
-      "sudo systemctl enable --now jenkins",
-      "sudo cat /var/lib/jenkins/secrets/initialAdminPassword",
+      "echo 'Waiting for instance to start...'",
+      "until curl http://${self.private_ip}:8080; do sleep 1; done",
+      "echo 'Instance is ready!'",
     ]
   }
 
-  depends_on = [aws_key_pair.deployer]
+  provisioner "local-exec" {
+  command = "ansible-playbook -i ${self.private_ip}, -u ${var.jenkins_admin_user} --private-key=${file("./deployer")} ansible/jenkins.yaml --extra-vars 'admin_user=${var.jenkins_admin_user} admin_password=${var.jenkins_admin_password}'"
+}
 
 }
 
 output "ubuntu" {
   value       = aws_instance.ubuntu.public_ip
-  description = "description"
+  description = "Public IP address of the Ubuntu instance"
 }
